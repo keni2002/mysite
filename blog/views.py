2a929d5e7e75237fe8427280020fc7df9d94d96a
keni2002 <yerbisuniverses@gmail.com>
2024-12-04 23:43:56 -0500

    add: email-system

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-
+from django.core.mail import send_mail
+from mysite import settings
 # Pagination
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
@@ -37,3 +38,29 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+from .forms import EmailPostForm
+
+def post_share(request,post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data 
+            #only clean the incorrect camp
+            #sending email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject,message,settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+
+    
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post':post,'form':form,'sent':sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -21,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Email server configuration
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER =  env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT=env('EMAIL_PORT')
+EMAIL_USE_TLS= not bool(env('EMAIL_USE_TLS'))
+EMAIL_USE_SSL= not bool(env('EMAIL_USE_SSL'))
