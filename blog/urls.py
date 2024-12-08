06a4a5c78e81f50f28499ba1d4ce091365eedbf6
keni2002 <yerbisuniverses@gmail.com>
2024-12-08 15:36:38 -0500

    add feed.xml + postgresql migrate

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .feeds import LatestPostsFeed
 from . import views
 app_name = 'blog'
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail, name='post_detail'),
     path('<int:post_id>/share/',views.post_share,name='post_share'),
     path('<int:post_id>/comment/',views.post_comment,name='post_comment'),
+    path('feed/',LatestPostsFeed(),name='post_feed'),
 
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -69,8 +69,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASS')
     }
 }
 
