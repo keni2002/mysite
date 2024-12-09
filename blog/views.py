125f0e1a45f5691d2a9923a3f213a31fa8db802d
keni2002 <yerbisuniverses@gmail.com>
2024-12-08 22:30:50 -0500

    + best search engine

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post,Comment
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from mysite import settings
 from taggit.models import Tag
 # Pagination
@@ -109,7 +109,14 @@ def post_search(request):
         form  = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title','body'),).filter(search=query)
+            search_vector = SearchVector('title',config='spanish',weight='A') + \
+                            SearchVector('body',weight='B')
+            search_query  = SearchQuery(query,config='spanish')
+
+
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query)
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request,'blog/post/search.html',{'form':form,
                                                        'query':query,
